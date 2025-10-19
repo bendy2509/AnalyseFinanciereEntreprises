f2bad325ab0a434f11c17ad63eb1e5568b3765b7
Jean Albikendy <albikendy.jean@student.ueh.edu.ht>
2025-10-19 07:39:54 -0400

    changement dans Program.cs et creation d'un nouvelle classe Utilitaires.cs

---
+++ AnalyseFinanciereEntreprises/services/Utilitaires.cs
@@ -0,0 +1,68 @@
+using System;
+    
+namespace AnalyseFinanciereEntreprises.services
+{
+    public static class Utilitaires
+    {
+        public static string LireSecteurValide()
+        {
+            string secteur;
+
+            while (true)
+            {
+                Console.Write("Secteur (technologie/sante/finance): ");
+                secteur = Console.ReadLine()?.Trim().ToLower();
+
+                if (secteur == "technologie" || secteur == "sante" || secteur == "finance")
+                {
+                    return secteur;
+                }
+
+                Console.WriteLine(" Choix invalide ! Veuillez saisir 'technologie', 'sante' ou 'finance'.");
+            }
+        }
+        
+        public static int LireEntier(string label)
+        {
+            int valeur;
+            Console.Write(label + ": ");
+            while (!int.TryParse(Console.ReadLine(), out valeur))
+            {
+                Console.WriteLine("Entrée invalide, réessayez : ");
+                Console.Write(label + ": ");
+            }
+            return valeur;
+        }
+    
+        public static string LireTexteNonVide(string label)
+        {
+            string saisie;
+            do
+            {
+                Console.Write(label + " : ");
+                saisie = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(saisie))
+                {
+                    Console.WriteLine("Ce champ est obligatoire !");
+                }
+
+            } while (string.IsNullOrWhiteSpace(saisie));
+
+            return saisie;
+        }
+
+        public static decimal LireDecimal(string label)
+        {
+            decimal valeur;
+            Console.Write(label + ": ");
+            while (!decimal.TryParse(Console.ReadLine(),out valeur))
+            {
+                Console.WriteLine("Entrée invalide, réessayez avec un decimal/nombre : ");
+                Console.Write(label + ": ");
+            }
+            return valeur;
+        }
+
+    }
+}
